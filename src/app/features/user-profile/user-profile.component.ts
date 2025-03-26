6e4e53f729cd9ceb3c6d7e1e73c7fdca71e89ba5
sondosomarr <sondosomarr2020@gmail.com>
2025-03-26 22:08:02 +0200

    Add InfoTable component and integrate it into the user profile layout

--- src/app/features/user-profile/user-profile.component.ts
+++ src/app/features/user-profile/user-profile.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from "../sidebar/sidebar.component";
+import { InfoTableComponent } from "../info-table/info-table.component";
 
 
 @Component({
   selector: 'app-user-profile',
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, InfoTableComponent],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
